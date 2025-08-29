92a29cfa06243e5cd2e038420d92e0db2733a801
Jeff Schmitz <jeffeschmitz@gmail.com>
2025-08-29 16:10:51 -0500

    chore: add initial Xcode SwiftUI scaffold (flattened layout)

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Jeffrey.Schmitz2 on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
